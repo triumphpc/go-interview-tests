5cea4aa2bcbcd0589a050253dac76db63fe84b86
Сергей Врулин <s.vrulin@vk.team>
2025-06-21 22:55:57 +0300

    up

--- test.go
+++ test.go
@@ -1,40 +1,20 @@
 package main
 
-import "fmt"
-
-type Order struct {
-	ID   int
-	Data string
-}
-
-var Results map[Order]string // Не инициировано
-
-func processOrders() {
-	orders := make(chan Order) // канал для заказов
-	//var results chan string // 1. Не иницирована
-	results := make(chan string) // канал для результатов
-
-	// Горутина для обработки заказов
-	go func() {
-		for order := range orders {
-			// Обработка заказа
-			result := fmt.Sprintf("Заказ %d обработан", order.ID)
-			results <- result
-
-			Results[order] = order.Data // 4. Тут гонка данных
-		}
-	}()
-
-	// Основной цикл
-	for i := 1; i <= 3; i++ { // 2. нужно выносить во вторую горутину
-		orders <- Order{ID: i, Data: "данные"}
-		// fmt.Println(<-results) // 1. будет deadlock
-	}
-
-	close(orders)
-	close(results) // 3. будет паника, если третья первая горутина не успеет все записать
-}
+import (
+	"log"
+	"slices"
+)
 
 func main() {
-	processOrders()
+	a := []int{1, 2, 3, 2}
+
+	for i, v := range a {
+		if v == 2 {
+			a = slices.Delete(a, i, i+1)
+
+			continue
+		}
+		log.Println(v, " ")
+	}
+
 }
